82a9cd47a2347d6c4fdb8b1af5d13f011d7efd66
Techihub99 <javatechihub@gmail.com>
2025-04-15 13:31:25 +0530

    test

---
+++ src/main/java/com/org/java/SpringbootTomcatCicdPipelineAppApplication.java
@@ -0,0 +1,19 @@
+package com.org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootTomcatCicdPipelineAppApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTomcatCicdPipelineAppApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootTomcatCicdPipelineAppApplication.class);
+	}
+
+}
